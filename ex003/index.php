b79615e7fa463f160b171d2dac54757a5e3491e0
Rafael Reis <105391575+RRRReiss@users.noreply.github.com>
2024-05-29 20:39:16 -0300

    ex003 feito

---
+++ ex003/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos em PHP</title>
+</head>
+<body>
+    <h1>Testes de Tipos Primitivos</h1>
+    <?php 
+        // 0x = hexadecimal; 0b = binário; 0 = octal
+        // $numero = 010;
+        // echo "O valor da variável é $numero";
+
+        // $var = "Rafael";
+        // var_dump($var);
+
+        // $num = (integer) 3e2; // (coerção) 3 x 10^2
+        // // echo "O valor é $num";
+        // var_dump($num);
+
+        // $num = (float)"950";
+        // var_dump($num);
+
+        // $casado = true;
+        // var_dump($casado);
+        // echo "O valor para casado é $casado"
+
+        // $vet = [6, 2.5, "Maria", 3, false];
+        // // echo "O vetor é $vet";
+        // var_dump($vet);
+
+        // class Pessoa {
+        //     private string $nome;
+        // }
+
+        // $p = new Pessoa;
+        // var_dump($p);
+    ?>
+</body>
+</html>
