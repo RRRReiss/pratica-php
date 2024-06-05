5ab9321e074ee46fbc6796bc71f56ef391bcf6ab
Rafael Reis <105391575+RRRReiss@users.noreply.github.com>
2024-06-05 19:07:04 -0300

    desafio 5 corrigido

--- desafio005/analise.php
+++ desafio005/analise.php
@@ -7,16 +7,16 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <section>
+    <main>
         <h1>Analisador de Número Real</h1>
         <?php 
-            $real = $_GET["real"];
+            $real = $_POST["real"] ?? 0;
             $inteiro = (int)$real;
             $fracionaria = $real - $inteiro;
-            echo "<p>Analisando o número $real informado pelo usuário:</p>";
-            echo "<ul><li>A parte inteira do número é $inteiro</li><li>A parte fracionário do número é $fracionaria</li></ul>";
+            echo "<p>Analisando o número <strong>".number_format($real, 3, ',', '.')." </strong>informado pelo usuário:</p>";
+            echo "<ul><li>A parte inteira do número é <strong>".number_format($inteiro, 0, '', '.')."</strong></li><li>A parte fracionário do número é <strong>".number_format($fracionaria, 3, ',', '.')."</strong></li></ul>";
         ?>
         <button type="button" onclick = "window.history.back();">Voltar</button>
-    </section>
+    </main>
 </body>
 </html>
