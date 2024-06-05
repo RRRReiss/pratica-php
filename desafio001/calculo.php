6a4af96a04b94cc97d3567c10b5ac51a11fda510
Rafael Reis <105391575+RRRReiss@users.noreply.github.com>
2024-06-05 15:40:04 -0300

    desafio 1 corrigido

--- desafio001/calculo.php
+++ desafio001/calculo.php
@@ -10,14 +10,12 @@
     <main>
         <h1>Resultado Final</h1>
         <?php 
-            $numero = $_GET["numero"];
+            $numero = $_GET["numero"] ?? 0;
             echo "<p>O número escolhido foi <strong>$numero</strong></p>";
-            $antecessor = $numero - 1;
-            echo "<p>O seu <i>antecessor</i> é $antecessor</p>";
-            $sucessor = $numero + 1;
-            echo "<p>O seu <i>sucessor</i> é $sucessor</p>";
+            echo "<p>O seu <i>antecessor</i> é " . ($numero - 1) . "</p>";
+            echo "<p>O seu <i>sucessor</i> é " . ($numero + 1) . "</p>";
         ?>
-        <button type="button" onclick="JavaScript: window.history.back();">← Voltar</button>
+        <button type="button" onclick="window.history.back();">&#x2B05; Voltar</button>
     </main>
 </body>
 </html>
