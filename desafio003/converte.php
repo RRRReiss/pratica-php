5388de4c9a136b16b75094603785da7d0c24dc74
Rafael Reis <105391575+RRRReiss@users.noreply.github.com>
2024-06-05 17:59:32 -0300

    desafio 3 corrigido

--- desafio003/converte.php
+++ desafio003/converte.php
@@ -7,17 +7,19 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <section>
+    <main>
         <h1>Conversor de Moedas v1.0</h1>
         <?php
-            $reais = $_GET["valor"];
+            $reais = $_GET["valor"] ?? 0;
             $dolares = $reais/5.29;
-            $reais = number_format($reais, 2,',','.');
-            $dolares = number_format($dolares, 2,',','.');
-            echo "<p>Seus R$  $reais equivalem a <strong>US$ $dolares</strong></p>";
-            echo "<p>*<strong>Cotação fixa de R$ 5,29</strong> informada diretamente no código.</p>"
+            // $reais = number_format($reais, 2,',','.');
+            // $dolares = number_format($dolares, 2,',','.');
+            $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+            // echo "<p>Seus R$  $reais equivalem a <strong>US$ $dolares</strong></p>";
+            // echo "<p>*<strong>Cotação fixa de R$ 5,29</strong> informada diretamente no código.</p>"
+            echo "<p>Seus " . numfmt_format_currency($padrao, $reais, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrao, $dolares, "USD") . "</strong></p>";
         ?>
         <button type="button" onclick="window.history.back();">Voltar</button>
-    </section>
+    </main>
 </body>
 </html>
